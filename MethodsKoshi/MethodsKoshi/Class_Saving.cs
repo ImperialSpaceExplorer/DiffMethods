a0a1317c68e51adf984adfec5ac3393fa23115fb
Анненко Миша 3821Б1ПИмэ <146870367+jazz931@users.noreply.github.com>
2024-12-21 02:24:23 +0300

    Update Class_Saving.cs
    починка метода Save

--- MethodsKoshi/MethodsKoshi/Class_Saving.cs
+++ MethodsKoshi/MethodsKoshi/Class_Saving.cs
@@ -12,33 +12,35 @@ namespace MethodsKoshi
         private List<double> hstep;
         private List<int> Nctr;
         private List<double> maxerr;
-        
         //Поменять путь Файла
         private string filePath = "C:\\Users\\wizze\\source\\repos\\Basalin _Lab\\";
-
+    
+        private bool IsMethod3 = false;
+    
         public Class_Saves
             (
             string File_Name,
             List<double> hstep,
             List<int> Nctr,
-            List<double> maxerr
+            List<double> maxerr,
+            bool IsMethod3
             )
         {
             filePath = filePath + File_Name;
             this.hstep = hstep;
             this.Nctr = Nctr;
             this.maxerr = maxerr;
+            this.IsMethod3 = IsMethod3;
         }
-
+    
         public void Save()
         {
             // Создаем объект StreamWriter для записи в файл
             using (StreamWriter writer = new StreamWriter(filePath))
             {
-
                 int n1 = hstep.Select(el => el.ToString().Length).Max();
                 int n2 = Nctr.Select(el => el.ToString().Length).Max();
-
+    
                 string skip = "              ";
                 writer.Write("h");
                 for (int i = 0; i < n1; i++)
@@ -53,16 +55,24 @@ namespace MethodsKoshi
                 for (int i = 0; i < n2; i++)
                 {
                     writer.Write(" ");
-                    if (i == n1 - 1)
+                    if (i == n2 - 1)
                     {
                         writer.Write(skip);
                     }
                 }
                 writer.Write("err");
                 writer.WriteLine();
-
+    
+    
+    
                 for (int i = 0; i < hstep.Count; i++)
                 {
+                    if (IsMethod3 && i % 4 == 0)
+                    {
+                        writer.WriteLine();
+                    }
+    
+    
                     writer.Write(hstep[i]);
                     if (hstep[i].ToString().Length < n1)
                     {
