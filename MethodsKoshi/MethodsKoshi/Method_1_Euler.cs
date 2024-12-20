1fa7974092f4c7694199cb2208e41e80bc8d811e
Анненко Миша 3821Б1ПИмэ <146870367+jazz931@users.noreply.github.com>
2024-12-21 00:48:48 +0300

    Update Method_1_Euler.cs
    Опять паттерн стратегии для
    private void ADDING_DATA_FOR_SAVE(double h, int Nctr, double err)

--- MethodsKoshi/MethodsKoshi/Method_1_Euler.cs
+++ MethodsKoshi/MethodsKoshi/Method_1_Euler.cs
@@ -8,15 +8,7 @@ namespace MethodsKoshi
 {
     class Method_1_Euler : Method
     {
-        //Добавление функции для сбора данных
-        private void ADDING_DATA_FOR_SAVE(double h, int Nctr, double err)
-        {
-            Hstep.Add(h);
-            NctR.Add(Nctr);
-            Maxerr.Add(err);
-        }
-
-        
+       
         double[] hstep = { 0.01, 0.001, 0.0001, 0.00001};
 
         public Method_1_Euler(List<string> exp) : base(exp) { }
