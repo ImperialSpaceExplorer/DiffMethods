b5378f6c48822aef96e61a9fbd94d3ea37de7bb9
Анненко Миша 3821Б1ПИмэ <146870367+jazz931@users.noreply.github.com>
2024-12-21 00:01:49 +0300

    Update IMethod.cs
    Добавление методов
    List<double> Get_H();
    List<int> Get_N();
    List<double> Get_Err();

--- MethodsKoshi/MethodsKoshi/IMethod.cs
+++ MethodsKoshi/MethodsKoshi/IMethod.cs
@@ -15,5 +15,10 @@ namespace MethodsKoshi
 
         void ToWriteConsole(double h, int Nctr, double err);  //for formalised output of each method in a console, add values to write in arguments
 
+
+        //мне пришлось вклинивать для нескольких переменных, которые используем в Сохранении
+        List<double> Get_H();
+        List<int> Get_N();
+        List<double> Get_Err();
     }
 }
